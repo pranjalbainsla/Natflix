8f87b7adfddfb25f3fab079bd23eebd54dcb80a6
Pranjal Bainsla <pranjalbainsla@kushagras-MacBook-Air-4.local>
2025-12-16 12:14:32 +0530

    fetching movies by popularity

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,62 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Search from "./components/Search"
+import Movie from "./components/Movie"
 
+const API_BASE_URL = 'https://api.themoviedb.org/3'
+
+const API_KEY = import.meta.env.VITE_TMDB_API_TOKEN
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  } 
+}
 
 const App = () => {
+  const [movies, setMovies] = useState([])
+  const [errorMessage, setErrorMessage] = useState("")
+
+  const fetchMovies = async () => {
+    try {
+      const res = await fetch(`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`, API_OPTIONS)
+
+      if(!res.ok){
+        //error
+        throw new Error('Failed to load movies')
+      }
+
+      const data = await res.json()
+      console.log(data)
+
+      setMovies(data.results)
+
+    } catch (error) {
+      console.log(error.message)
+      setErrorMessage("Error loading movies!")
+    }
+  }
   useEffect(()=>{
-    
+    fetchMovies();
   }, [])
   return (
     <main>
-      <div className="header">Find <span className="purple-gradient">Movies</span> You'll Enjoy Without The Hassle</div>
-      <Search /> 
+      <header>
+        <div className="header">Find <span className="purple-gradient">Movies</span> You'll Enjoy Without The Hassle</div>
+        <Search /> 
+      </header>
+
+      <section>
+        <h2>all movies</h2>
+        <ul className="movie-list">
+          {movies.map((movie, index)=>(
+            <Movie id={index} movie={movie}/>
+          ))}
+        </ul>
+        {errorMessage? <p>{errorMessage}</p> : null}
+      </section>
+      
     </main>
   )
 }
---
+++ src/components/Movie.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import heartIcon from "../assets/heart.svg";
+
+const Movie = ({ movie : { title, poster_path, release_date, vote_average, original_language } }) => {
+    return (
+        <div className="movie-card">
+            <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt='poster missing' />
+            <div>
+                <div className="movie-title">{title}</div>
+                <div className="movie-info">
+                    <img src={heartIcon} />
+                    <span className="rating">{vote_average ? vote_average.toFixed(1) : 'N/A'}</span>
+                    <span>•</span>
+                    <span>{original_language ? original_language : 'N/A'}</span>
+                    <span>•</span>
+                    <span>{release_date ? release_date.split('-')[0] : 'N/A'}</span>
+                </div>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Movie
