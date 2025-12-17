afc37063ec6f9eddb7a8efe35930a631affc8419
Pranjal Bainsla <pranjalbainsla@kushagras-MacBook-Air-4.local>
2025-12-17 22:35:59 +0530

    vite warning

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000, // in KB (default is 500)
+  },
 })
 //this file basically allows to you customize the build process, such as adding plugins, configuring server setting etc
